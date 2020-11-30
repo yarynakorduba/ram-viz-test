b3c4cb0295ab547cb8d6b1197e2100e0df628cf1
yarynakorduba <yaryna.korduba@keenethics.com>
2020-12-01 01:59:28 +0200

    Modify Analytics trackers.

--- src/helpers/analyticsTrackers.js
+++ src/helpers/analyticsTrackers.js
@@ -1,43 +1,57 @@
-const generateMatomoSnippet = () => `
-  <!-- Matomo -->
-  <script type="text/javascript">
-    var _paq = window._paq = window._paq || [];
-    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-    _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-    _paq.push(["setCookieDomain", "*.yarynakorduba.github.io"]);
-    _paq.push(["setDomains", ["*.yarynakorduba.github.io/ram-viz"]]);
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function() {
-      var u="https://localhostlocal.matomo.cloud/";
-      _paq.push(['setTrackerUrl', u+'matomo.php']);
-      _paq.push(['setSiteId', '2']);
-      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-      g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/localhostlocal.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
-    })();
-  </script>
-  <noscript><p><img src="https://localhostlocal.matomo.cloud/matomo.php?idsite=2&amp;rec=1" style="border:0;" alt="" /></p></noscript>
-  <!-- End Matomo Code -->   
-`;
+const createMatomoSnippet = (first, rest) => {
+  var _paq = window._paq || [];
+  var trackingUrl = first.trackingUrl;
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["trackPageView"]);
+  _paq.push(["enableLinkTracking"]);
 
-const generateGoogleAnalyticsSnippet = () => `
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-F4051947VB"></script>
-<script>
+  _paq.push(["setTrackerUrl", trackingUrl + "matomo.php"]);
+  _paq.push(["setSiteId", first.trackingId]);
+  _.map(rest, (tracker) => _paq.push(["addTracker", tracker.trackingUrl + "matomo.php", tracker.trackingId]));
+  const d = document;
+  const g = d.createElement("script");
+  const s = d.getElementsByTagName("script")[0];
+  g.type = "text/javascript";
+  g.async = true;
+  g.defer = true;
+  g.src = trackingUrl + "matomo.js";
+  s.parentNode.insertBefore(g, s);
+};
+
+const createGoogleAnalyticsSnippet = () => {
+  const d = document;
+  const g = d.createElement("script");
+  const s = d.getElementsByTagName("script")[0];
+  g.async = true;
+  g.src = "https://www.googletagmanager.com/gtag/js?id=G-F4051947VB";
+  s.parentNode.insertBefore(g, s);
+
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
 
-  gtag('config', 'G-F4051947VB');
-</script>
-`;
+  gtag("config", "G-F4051947VB");
+};
+
+// const generateGoogleAnalyticsSnippet = () => `
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-F4051947VB"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-F4051947VB');
+// </script>
+// `;
 
 export default function injectTrackers() {
   try {
-    const googleTrackersSnippet = generateMatomoSnippet();
-    const matomoTrackersSnippet = generateGoogleAnalyticsSnippet();
-    document.head.insertAdjacentHTML("afterbegin", googleTrackersSnippet);
-    document.head.insertAdjacentHTML("afterbegin", matomoTrackersSnippet);
+    const matomoTrackers = [{ trackingId: "2", trackingUrl: "https://localhostlocal.matomo.cloud/" }];
+    createMatomoSnippet(matomoTrackers, []);
+    createGoogleAnalyticsSnippet();
   } catch (error) {
     console.log("Trackers could not be added", error);
   }

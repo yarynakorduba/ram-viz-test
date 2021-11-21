37ca9946e86e2210d98c840b1ca358cb5e315752
Yaryna Korduba <yaryna.korduba@cordian.com>
2021-11-21 13:12:56 +0100

    Add article and style it.

--- src/components/RamVisualization/RamVisualization.js
+++ src/components/RamVisualization/RamVisualization.js
@@ -12,22 +12,24 @@ const b = BEM("RAMVisualization");
 const RAMVisualization = () => {
   return (
     <div className={b()}>
-      <div className={b("caption")}>
-        <h1 className={b("header")}>Random Access Memory [RAM]</h1>
-        <VisualizationSettings />
-      </div>
-      <div className={b("content")}>
-        <div className={b("pins")}>
-          <MemoryControls />
+      <div className={b("container")}>
+        <header className={b("caption")}>
+          <h2>RAM Playground</h2>
+          <VisualizationSettings />
+        </header>
+        <div className={b("content")}>
+          <div className={b("pins")}>
+            <MemoryControls />
+          </div>
+          <div className={b("memory")}>
+            <Memory />
+          </div>
         </div>
-        <div className={b("memory")}>
-          <Memory />
+        <div className={b("notes")}>
+          <div>＊ LSB - Least Significant Bit (right-most bit)</div>
+          <div>＊ MSB - Most Significant Bit (left-most bit)</div>
         </div>
       </div>
-      {/* <div className={b("notes")}>
-        <div>＊ LSB - Least Significant Bit (right-most bit)</div>
-        <div>＊ MSB - Most Significant Bit (left-most bit)</div>
-      </div> */}
     </div>
   );
 };

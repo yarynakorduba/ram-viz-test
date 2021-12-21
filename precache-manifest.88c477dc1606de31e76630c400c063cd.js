d6d684cacd5dc077ca48432906fdca8be270d9f3
Yaryna Korduba <yaryna.korduba@cordian.com>
2021-12-21 02:31:14 +0200

    Updates

---
+++ precache-manifest.88c477dc1606de31e76630c400c063cd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6a690047af1fd1a338d8baddaa18186",
+    "url": "/ram-viz-test/index.html"
+  },
+  {
+    "revision": "2267fd9ed330a98a9a41",
+    "url": "/ram-viz-test/static/css/main.f2073a17.chunk.css"
+  },
+  {
+    "revision": "41ec46ac586b2eadabb1",
+    "url": "/ram-viz-test/static/js/2.365dcf98.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ram-viz-test/static/js/2.365dcf98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2267fd9ed330a98a9a41",
+    "url": "/ram-viz-test/static/js/main.bfc68b97.chunk.js"
+  },
+  {
+    "revision": "5999daf4a4c13f3e852c",
+    "url": "/ram-viz-test/static/js/runtime-main.e94df4a6.js"
+  },
+  {
+    "revision": "23c42a437fee4e4bee62d52fa42c68b0",
+    "url": "/ram-viz-test/static/media/memoryBank.23c42a43.svg"
+  },
+  {
+    "revision": "dc6a5e8d54e5a23bb29ea1c054b0a1c7",
+    "url": "/ram-viz-test/static/media/memoryBankRasCas.dc6a5e8d.svg"
+  },
+  {
+    "revision": "4feac339500567906d9d752edeccfea3",
+    "url": "/ram-viz-test/static/media/memoryBankRowCol.4feac339.svg"
+  },
+  {
+    "revision": "7657086e8f7f9658a650abaab831516c",
+    "url": "/ram-viz-test/static/media/memoryBlock.7657086e.svg"
+  },
+  {
+    "revision": "31663eda483234b17aeb829c8c4a01d9",
+    "url": "/ram-viz-test/static/media/memoryBlocks.31663eda.svg"
+  },
+  {
+    "revision": "375091a1e8d019052d7885ddeacdc78e",
+    "url": "/ram-viz-test/static/media/memoryCellCircuit.375091a1.svg"
+  }
+]);
